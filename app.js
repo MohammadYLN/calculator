3dda830a06dda22df989cae889c85905ee8751b4
Mohammad Yalaan <mohamadyalaan@gmail.com>
2020-03-10 15:23:56 +0330

    adding add subtract divide multiply functions

--- app.js
+++ app.js
@@ -6,9 +6,39 @@ function parseInput(){
     return parseInt(userInput.value);
 }
 
+
+
+
 function createAndWriteOutput(operator,beforeResult,calcNum){
     const calcDesc = `${beforeResult} ${operator} ${calcNum}`;
     outputResult(currentResult,calcDesc); 
+}
+
+function calculateResult(calculationType){
+    const enteredNum = parseInt();
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType !== "AND" && calculationType !== "DIVIDE"
+        && calculationType !== "SUBTRACT" && calculationType !== "MULTIPLY"
+        || !enteredNum){
+            return;
+        }
+        if (calculationType === "AND"){
+            currentResult += enteredNum;
+            mathOperator = "+";
+        }
+        else if (calculationType === "SUBTRACT"){
+            currentResult -= enteredNum;
+            mathOperator = "-";
+        }
+        else if (calculationType === "DIVIDE"){
+            currentResult /= enteredNum;
+            mathOperator = "/";
+        }
+        else if (calculationType === "MULTIPLY"){
+            currentResult *= enteredNum;
+            mathOperator = "*";
+        }
 }
 
 function createAndPushLog(operator,number,prevResult){
